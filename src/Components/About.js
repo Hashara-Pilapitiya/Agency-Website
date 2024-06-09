28dedf9f57733a007e9b8407ac98ef05a2e39c3c
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-06-09 18:26:53 +0530

    Add 'about' and 'testimonial' section IDs for better navigation

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,10 +7,10 @@ import { MdOutlinePayments } from "react-icons/md";
 
 const About = () => {
   return (
-    <div>
+    <div id='about'>
 
         {/* about text */}
-        <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto my-8' id='about'>
+        <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto my-8'>
             <div className='md:w-11/12 mx-auto flex flex-col md:flex-row justify-between items-center gap-12'>
 
                 <div>
